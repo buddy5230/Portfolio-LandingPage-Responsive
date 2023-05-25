9046b41f1f666df6ed5a5d8ef459d867b7a3f4bc
buddy5230 <74503111+buddy5230@users.noreply.github.com>
2023-05-25 23:25:39 +0700

    fix contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,43 +5,46 @@ import { MdEmail } from "react-icons/md";
 import { BsFillPhoneFill } from "react-icons/bs";
 
 const Contact: React.FC = () => {
-
   return (
     <>
-    <h1>Contact</h1>
-    <div className="con-contact" id="contact">
-      <div className="box-contact">
-        <MdEmail className="icon"></MdEmail>
-        <div className="set-text">
-          <h2>Email</h2>
-          <p>poopirat.buddy@gmail.com</p>
+      <h1>Contact</h1>
+      <div className="con-contact" id="contact">
+        <div className="box-contact">
+          <MdEmail className="icon"></MdEmail>
+          <div className="set-text">
+            <h2 className="email">Email</h2>
+            <p>poopirat.buddy@gmail.com</p>
+          </div>
+        </div>
+        <div className="box-contact">
+          <a href="https://www.facebook.com/poopirat.buddy/">
+            <AiFillFacebook className="icon"></AiFillFacebook>
+          </a>
+          <div className="set-text">
+            <h2 className="face">Facebook</h2>
+            <p>B'uddy Natthanasak</p>
+          </div>
+        </div>
+        <div className="box-contact">
+          <BsFillPhoneFill className="icon"></BsFillPhoneFill>
+          <div className="set-text">
+            <h2 className="phone">Phone</h2>
+            <p>093-3186594</p>
+          </div>
+        </div>
+
+        <div className="box-contact">
+          <a href="https://github.com/buddy5230">
+            <AiFillGithub className="icon"></AiFillGithub>
+          </a>
+          <div className="set-text">
+            <h2 className="git">Github</h2>
+            <p>https://github.com/buddy5230</p>
+          </div>
         </div>
       </div>
-      <div className="box-contact">
-        <a href="https://www.facebook.com/poopirat.buddy/"><AiFillFacebook className="icon"></AiFillFacebook></a>
-        <div className="set-text">
-          <h2>Facebook</h2>
-          <p>B'uddy Natthanasak</p>
-        </div>
-      </div>
-      <div className="box-contact">
-        <BsFillPhoneFill className="icon"></BsFillPhoneFill>
-        <div className="set-text">
-          <h2>Phone</h2>
-          <p>093-3186594</p>
-        </div>
-      </div>
-      
-      <div className="box-contact">
-        <a href="https://github.com/buddy5230"><AiFillGithub className="icon"></AiFillGithub></a>
-        <div className="set-text">
-          <h2>Github</h2>
-          <p>https://github.com/buddy5230</p>
-        </div>
-      </div>
-    </div>
     </>
   );
-}
+};
 
 export default Contact;
